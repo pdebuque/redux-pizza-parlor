af1467c567d516a545a22aa24d7798c43969eebc
Victor Llapa <victorllapa@gmail.com>
2022-12-01 13:28:43 -0600

    Have completed the admin details page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,44 +1,31 @@
-
 import React from "react";
 import axios from "axios";
 import "./App.css";
 import { HashRouter as Router, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
-
+import Admin from "../Admin/Admin.jsx";
 
 function App() {
 	return (
 		<div className='App'>
 			<header className='App-header'>
 				<h1 className='App-title'>Prime Pizza</h1>
+				<p>cart price</p>
 			</header>
-
-
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Prime Pizza</h1>
-        <p>cart price</p>
-      </header>
-      <Router>
-
-        <Route exact path="/">
-          <Menu />
-        </Route>
-        <Route exact path="/customer">
-          <Customer />
-        </Route>
-        <Route exact path="/checkout">
-          <Checkout />
-        </Route>
-        <Route exact path="/admin">
-          <Admin />
-        </Route>
-
-      </Router>
-      <img src='images/pizza_photo.png' />
-      <p>Pizza is great.</p>
-
+			<Router>
+				<Route exact path='/'>
+					{/* <Menu /> */}
+				</Route>
+				<Route exact path='/customer'>
+					{/* <Customer /> */}
+				</Route>
+				<Route exact path='/checkout'>
+					{/* <Checkout /> */}
+				</Route>
+				<Route exact path='/admin'>
+					<Admin />
+				</Route>
+			</Router>
 
 			{/* 
       
--- src/index.js
+++ src/index.js
@@ -1,42 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App/App';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App/App";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import { Provider } from "react-redux";
 
-const currentOrder = (state = {
-    customer_name: "",
-    street_address: "",
-    city: "",
-    zip: "",
-    total: 0,
-    type: "",
-    pizzas: []
-}, action) => {
-    if (action.type === "ADD_PIZZAS") {
-        return { ...state, pizzas: action.payload }
-    }
-    if (action.type === "SET_CUSTOMER") {
-        const customer = action.payload
-        return {
-            ...state,
-            customer_name: customer.name,
-            street_address: customer.address,
-            city: customer.city,
-            zip: customer.zip,
-        }
-    }
-    return state
-}
+const currentOrder = (
+	state = {
+		customer_name: "",
+		street_address: "",
+		city: "",
+		zip: "",
+		total: 0,
+		type: "",
+		pizzas: [],
+	},
+	action
+) => {
+	if (action.type === "ADD_PIZZAS") {
+		return { ...state, pizzas: action.payload };
+	}
+	if (action.type === "SET_CUSTOMER") {
+		const customer = action.payload;
+		return {
+			...state,
+			customer_name: customer.name,
+			street_address: customer.address,
+			city: customer.city,
+			zip: customer.zip,
+		};
+	}
+	return state;
+};
 
-const store = createStore(combineReducers({
-    currentOrder
-}),
-    applyMiddleware(logger))
+const store = createStore(
+	combineReducers({
+		currentOrder,
+	}),
+	applyMiddleware(logger)
+);
 
 ReactDOM.render(
-    <Provider>
-        <App />
-    </Provider>, document.getElementById('root'));
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
+);
