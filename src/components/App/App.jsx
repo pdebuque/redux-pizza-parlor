7cc411c1d80c04ccb898c53243199893ce1f704c
Brett R. Gebbie <brg078@gmail.com>
2022-12-01 10:13:34 -0600

    skeleton router

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { Route, Router } from 'react-router-dom';
 
 function App() {
 
@@ -8,8 +9,24 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
+        <p>cart price</p>
       </header>
-  
+      <Router>
+
+        <Route exact path="/">
+          <Menu />
+        </Route>
+        <Route exact path="/customer">
+          <Customer />
+        </Route>
+        <Route exact path="/checkout">
+          <Checkout />
+        </Route>
+        <Route exact path="/admin">
+          <Admin />
+        </Route>
+
+      </Router>
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
   
