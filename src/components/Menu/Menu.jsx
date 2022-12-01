c3da6b65e3da6b6381d28805cac64730a426993a
Paolo Debuque <pdebuque@gmail.com>
2022-12-01 11:32:57 -0600

    add menu button

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,17 +1,29 @@
 import { useState } from 'react';
-import { useDispatch,useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import PizzaListItem from '../PizzaListItem/PizzaListItem';
 
 
 export default function Menu() {
 
-const pizzaList = useSelector(store=>store.pizzaList);
+    const dispatch = useDispatch();
+    const [newOrder, setOrder] = useState([])
+    const pizzaList = useSelector(store => store.pizzaList);
 
-    return(
-        pizzaList.map(pizza=>{
-            return(
-                <div key = {pizza.id}>{pizza.name}</div>
-                )
-        })
+    const submitOrder = () => {
+        // take the order and send it to redux
+        console.log('in submitOrder')
+        dispatch({type: 'SET_ORDER',payload: newOrder})
+    }
+
+    return (
+        <main>
+            {JSON.stringify(newOrder)}
+            {pizzaList.map(pizza => {
+                return (<PizzaListItem pizza={pizza} setOrder={setOrder} newOrder={newOrder} />)
+            })}
+            <button onClick={submitOrder}>submit</button>
+        </main>
+
     )
 }
