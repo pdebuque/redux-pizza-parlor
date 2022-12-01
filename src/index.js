0c30c4e480c94ced708e10048e6285708e455eda
Paolo Debuque <pdebuque@gmail.com>
2022-12-01 10:03:16 -0600

    redux set up

--- src/index.js
+++ src/index.js
@@ -2,5 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const currentOrder = (state = {
+    customer_name: "",
+    street_address: "",
+    city: "",
+    zip: "",
+    total: 0,
+    type: "",
+    pizzas: []
+}, action) => {
+    if (action.type === "ADD_PIZZAS") {
+        return { ...state, pizzas: action.payload }
+    }
+    if (action.type === "SET_CUSTOMER") {
+        const customer = action.payload
+        return {
+            ...state,
+            customer_name: customer.name,
+            street_address: customer.address,
+            city: customer.city,
+            zip: customer.zip,
+        }
+    }
+    return state
+}
+
+const store = createStore(combineReducers({
+    currentOrder
+}),
+    applyMiddleware(logger))
+
+ReactDOM.render(
+    <Provider>
+        <App />
+    </Provider>, document.getElementById('root'));
